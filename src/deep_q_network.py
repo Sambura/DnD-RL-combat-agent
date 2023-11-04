33ffe91ef584b3492505bbf964e2225fd2663929
Kirill Samburskiy <superkirs2@gmail.com>
2023-11-04 03:40:59 +0300

    Improvements/refactoring/documentation

--- src/deep_q_network.py
+++ src/deep_q_network.py
@@ -1,15 +1,16 @@
 from torch import nn
 
 class DnDEvalModel(nn.Module):
-    def __init__(self, in_layers, out_layers):
+    def __init__(self, in_layers: int, out_layers: int, ):
         super(DnDEvalModel, self).__init__()
+        relu_slope = 0.003
         self.layers = nn.Sequential(
             nn.Conv2d(in_layers, 16, 3, padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(relu_slope),
             nn.Conv2d(16, 64, 5, padding=2),
-            nn.ReLU(),
+            nn.LeakyReLU(relu_slope),
             nn.Conv2d(64, 16, 5, padding=2),
-            nn.ReLU(),
+            nn.LeakyReLU(relu_slope),
             nn.Conv2d(16, out_layers, 3, padding=1),
         )
 
--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,69 @@
+import matplotlib.pyplot as plt
+from math import ceil, sqrt
 import numpy as np
 import random
-from math import ceil, sqrt
-import matplotlib.pyplot as plt
 
-def manhattan_distance(point1, point2):
+from typing import Union, Optional
+
+IntPoint2d = Union[tuple[int, int], np.ndarray, list[int]]
+
+def manhattan_distance(point1: IntPoint2d, point2: IntPoint2d) -> int:
+    """Calculate Manhattan distance between two points"""
     x1, y1 = point1
     x2, y2 = point2
     return abs(x1 - x2) + abs(y1 - y2)
 
-def to_tuple(coords):
+def to_tuple(coords: IntPoint2d) -> tuple[int, int]:
+    """Convert from list/ndarray coordinates representation to tuple of ints"""
     return tuple(np.array(coords).flatten())
 
-def get_random_coords(h, w):
-    return (random.randrange(0, h), random.randrange(0, w))
+def get_random_coords(max_y: int, max_x: int) -> tuple[int, int]:
+    """Get a random coordinate on a board of size (max_y, max_x)"""
+    return (random.randrange(0, max_y), random.randrange(0, max_x))
 
-def plot_featuremaps(data, name="", fm_names=None, vmin=None, vmax=None, shape=None, separate_cbars=False, show=True, cmap='coolwarm'):
+def plot_featuremaps(data: np.ndarray,
+                     title: Optional[str]=None,
+                     fm_names: Optional[list[str]]=None,
+                     vmin: Optional[float]=None,
+                     vmax: Optional[float]=None,
+                     shape: Optional[tuple[int, int]]=None,
+                     separate_cbars: bool=False,
+                     show: bool=True,
+                     cmap: str='coolwarm',
+                     cell_size: float=2.5) -> Optional[tuple[plt.Figure, np.ndarray]]:
+    """
+    Plots a stack of 2D featuremaps
+
+    Parameters:
+    data (ndarray): ndarray of 2D featuremaps with shape [N, H, W], where N - number of 
+        featuremaps, H - height, W - width
+    title (str): Title of the plot
+    fm_names (list[str]) list of length N, with names of the featuremaps in `data`
+    vmin, vmax: refer to plt.colorbar()
+    shape (tuple[int, int]): Size of grid where featuremap will be plotted
+    separate_cbars (bool): If True, each featuremap will have its own colorbar, 
+        and `vmin` & `vmax` parameters will be ignored
+    show: (bool): If True, plt.show() is called. Otherwise tuple (fig, axes) is returned
+    cmap (str): colormap to use
+    cell_size (float): basically represents figure size
+
+    Returns:
+    Tuple of (Figure, axes) if `show` == False, otherwise None
+    """
     if shape is None:
         width = int(ceil(sqrt(data.shape[0])))
         height = int(ceil(data.shape[0] / width))
         shape = (height, width)
     
-    if vmin is None and not separate_cbars: vmin = np.min(data)
-    if vmax is None and not separate_cbars: vmax = np.max(data)
+    if separate_cbars: 
+        vmin = None
+        vmax = None
+    else:
+        if vmin is None: vmin = np.min(data)
+        if vmax is None: vmax = np.max(data)
 
-    fig, ax_m = plt.subplots(*shape, dpi=100)
-    fig.suptitle(name)
+    fig, ax_m = plt.subplots(*shape, dpi=100, figsize=np.array(shape[::-1]) * cell_size)
+    if title is not None: fig.suptitle(title)
     axs = np.asarray(ax_m).ravel()
     imgs = []
     for i, (fm, ax) in enumerate(zip(data, axs)):
@@ -41,3 +80,6 @@ def plot_featuremaps(data, name="", fm_names=None, vmin=None, vmax=None, shape=N
     if show: 
         if separate_cbars: plt.tight_layout()
         plt.show()
+        return
+    
+    return fig, ax_m
